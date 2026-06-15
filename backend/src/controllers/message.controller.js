3cab73578cf6bb88c019bdd7512511472bba4f1f
Mohit Singh <mohitsinghinsecondary@gmail.com>
2026-06-15 20:44:05 +0530

    Fix import path for User model in message.controller.js

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,4 +1,4 @@
-import User from "./models/user.model.js";
+import User from "../models/user.model.js";
 import { hasImageKitConfig, uploadChatMedia } from "../lib/imagekit.js";
 
 export async function getUserForSidebar(req, res) {
