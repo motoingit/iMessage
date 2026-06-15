2f986f0b346c35ee878968e02ce24dce94ae7a33
Mohit Singh <mohitsinghinsecondary@gmail.com>
2026-06-15 20:49:28 +0530

    Fix import statement for message controller and correct sendMessages function reference in message.route.js

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {getUserForSidebar} from '../controllers/message.controller.js';
+import {getUserForSidebar, getConversationsForSidebar, getMessages, sendMessages} from '../controllers/message.controller.js';
 import { protectRoute } from '../middleware/auth.middleware.js';
 import {upload } from '../middleware/upload.middleware.js';
 
@@ -12,7 +12,7 @@ router.get("/users" ,getUserForSidebar)
 router.get("/conversations" ,getConversationsForSidebar)
 router.get("/:id" ,getMessages)
 
-router.post("/send/:id", upload.single("media"), upload,sendMessage)
+router.post("/send/:id", upload.single("media"), upload,sendMessages)
 //todo: show this in the frotend
 
 export default router;
