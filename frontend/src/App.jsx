1c7d9a05e9b3542c317f28989302fe39469d9e75
Mohit Singh <mohitsinghinsecondary@gmail.com>
2026-06-16 11:10:26 +0530

    feat: integrate theme and wallpaper context providers, add routing for authentication and chat pages
    - Added ThemeProvider and WallpaperProvider for managing themes and wallpapers.
    - Implemented routing using react-router for navigating between AuthPage and ChatPage.
    - Updated App component to utilize authentication state for conditional rendering.
    - Removed unused App.css and integrated styles from HeroUI.
    - Added new assets including wallpapers and audio files.
    - Created herouiThemePresets and wallpapers data structures for theme and wallpaper management.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,47 @@
-import './App.css'
 
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+
+import { ThemeProvider } from './context/ThemeContext';
+import { WallpaperProvider } from './context/WallpaperContext';
+
+import { useAuth } from '@clerk/react';
+import {Navigate, Route, Routes} from 'react-router';
+
+import ChatPage from './pages/ChatPage';
+import AuthPage from './pages/AuthPage';
 
 function App() {
+
+  const{isSignedIn, isLoaded} = useAuth();
+
   return (
-    <>
-      <h1>hello</h1>
-
-      <header>
-        <Show when="signed-out">
-          <SignInButton mode='modal'/>
-          <SignUpButton mode='modal'/>
-        </Show>
-
-        <Show when="signed-in">
-          <UserButton mode='modal'/>
-        </Show>
-      </header>
-    </>
+    <ThemeProvider>
+      <WallpaperProvider>
+        <Routes>
+          <Route path='/' element={isSignedIn ? <ChatPage /> : <Navigate to="/auth" replace />} />
+          <Route path='/auth' element={!isSignedIn ? <AuthPage /> : <Navigate to="/chat" replace />} />
+        </Routes>
+      </WallpaperProvider>
+    </ThemeProvider>
   )
 }
 
-export default App
+export default App;
+
+
+/*
+import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+import { Button } from '@heroui/react';
+
+<h1 className='text-4xl text-red-500 bg-blue-500'>hello</h1>
+<Button> My Button </Button>
+<header>
+  <Show when="signed-out">
+    <SignInButton mode='modal'/>
+    <SignUpButton mode='modal'/>
+  </Show>
+
+  <Show when="signed-in">
+    <UserButton mode='modal'/>
+  </Show>
+</header>
+ */
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,13 +3,16 @@ import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ClerkProvider } from '@clerk/react'
+import { BrowserRouter } from "react-router-dom";
 
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ClerkProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ClerkProvider>
   </StrictMode>,
 )
